401037ec9de99dbfef19370e4319f500e8dd52f4
Nisha Singh <2428051@kiit.ac.in>
2026-03-04 20:50:02 +0000

    first commit

---
+++ electricitybill.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if(units <= 100)
+        bill = units * 1.5;
+    else if(units <= 300)
+        bill = 100*1.5 + (units-100)*2.5;
+    else
+        bill = 100*1.5 + 200*2.5 + (units-300)*4;
+
+    printf("Total Bill = %.2f\n", bill);
+
+    return 0;
+}
